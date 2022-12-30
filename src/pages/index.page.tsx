56cde4cf7933c74a9fa41bd834c7408a82643828
Lucas Vaz <httpslucasvaz@gmail.com>
2022-12-30 00:55:21 -0300

    feat: add localStorage for task load

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -5,6 +5,8 @@ import {
   Typography,
   Button,
   Divider,
+  Alert,
+  Snackbar,
 } from '@mui/material'
 
 import Tab from '@mui/material/Tab'
@@ -13,21 +15,52 @@ import TabList from '@mui/lab/TabList'
 import TabPanel from '@mui/lab/TabPanel'
 
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import ClearIcon from '@mui/icons-material/Clear'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
-import { useState } from 'react'
+import UndoIcon from '@mui/icons-material/Undo'
+import DoDisturbAltIcon from '@mui/icons-material/DoDisturbAlt'
+import GradingIcon from '@mui/icons-material/Grading'
+import AssignmentIcon from '@mui/icons-material/Assignment'
+import { useEffect, useState } from 'react'
+
+import dynamic from 'next/dynamic'
 
 interface ObjProps {
   id: number
   description: string
-  done: boolean
+  taskValue: string
 }
 
-export default function Home() {
-  const [task, setTask] = useState<ObjProps[]>([])
-  const [taskDone, setTaskDone] = useState<ObjProps[]>([])
+function Home() {
+  const [task, setTask] = useState<ObjProps[]>(() => {
+    const savedTask: any = localStorage.getItem('key')
+    const parsedTask = JSON.parse(savedTask)
+    return parsedTask || ''
+  })
   const [input, setInput] = useState('')
   const [value, setValue] = useState('1')
+  /// //////
+  const [open, setOpen] = useState(false)
+
+  const handleSnackClick = () => {
+    setOpen(true)
+  }
+
+  const handleSnackClose = (
+    // eslint-disable-next-line no-undef
+    event?: React.SyntheticEvent | Event,
+    reason?: string,
+  ) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('key', JSON.stringify(task))
+  }, [task])
 
   const handleChange = (e: any, newValue: string) => {
     setValue(newValue)
@@ -41,7 +74,7 @@ export default function Home() {
       const obj: ObjProps = {
         id: Date.now(),
         description: input,
-        done: false,
+        taskValue: '1',
       }
       setTask([...task, obj])
       setInput('')
@@ -49,17 +82,41 @@ export default function Home() {
   }
 
   const handleTaksDone = (tasks: ObjProps) => {
-    const currentTask = task.find((item) => {
+    const currentTask: any = task.find((item) => {
+      return item.id === tasks.id
+    })
+    currentTask.taskValue = '2'
+    setTask([...task])
+    handleSnackClick()
+  }
+
+  const handleTaksCancel = (tasks: ObjProps) => {
+    const currentTask: any = task.find((item) => {
       return item.id === tasks.id
     })
 
-    currentTask.done = !currentTask.done
+    currentTask.taskValue = '3'
     setTask([...task])
+  }
 
-    const filterTaskDone = task.filter((item) => item.done === true)
+  const handleUndo = (tasks: ObjProps) => {
+    const currentTask: any = task.find((item) => {
+      return item.id === tasks.id
+    })
 
-    setTaskDone([...taskDone, filterTaskDone])
-    console.log(taskDone)
+    currentTask.taskValue = '1'
+    setTask([...task])
+  }
+
+  const handleKeyPress = (e: any) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      handleAddTask()
+    }
+  }
+
+  const handleResetTask = () => {
+    setTask([])
   }
 
   return (
@@ -67,19 +124,32 @@ export default function Home() {
       sx={{
         backgroundColor: '#e0e0de',
         width: '100%',
-        height: '100vh',
+        minHeight: '100vh',
       }}
     >
       <Container maxWidth="md">
-        <Typography
-          variant="h2"
-          sx={{
-            color: '#40403F',
-            fontWeight: 800,
-          }}
-        >
-          MY TODO
-        </Typography>
+        <Box>
+          <Typography
+            variant="h2"
+            sx={{
+              color: '#40403F',
+              fontWeight: 800,
+            }}
+          >
+            MY
+          </Typography>
+          <Typography
+            variant="h1"
+            sx={{
+              color: '#40403F',
+              fontWeight: 800,
+              mt: '-20px',
+            }}
+          >
+            {' '}
+            TODO
+          </Typography>
+        </Box>
         <Box display="flex" sx={{ mb: 4 }}>
           <TextField
             fullWidth
@@ -90,6 +160,7 @@ export default function Home() {
             focused
             value={input}
             onChange={(e) => setInput(e.target.value)}
+            onKeyPress={handleKeyPress}
           />
           <Button
             variant="contained"
@@ -101,9 +172,7 @@ export default function Home() {
           </Button>
         </Box>
         <Divider />
-
         {/* //////// HEADER */}
-
         <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
             <TabList
@@ -124,73 +193,331 @@ export default function Home() {
               <Tab label="CANCELADOS" value="3" />
             </TabList>
           </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              mt: 1,
+            }}
+          >
+            <Button
+              variant="contained"
+              size="small"
+              color="secondary"
+              onClick={handleResetTask}
+            >
+              RESET
+            </Button>
+          </Box>
           <TabPanel value="1">
-            {task?.map((task, index) => {
-              return (
-                <>
-                  <Box display="flex">
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        width: '100%',
-                        minHeight: '4rem',
-                        borderRadius: '10px',
-                        backgroundColor: '#cfcfcf',
-                        borderLeft: '10px solid red',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <Box
-                        display="flex"
-                        sx={{
-                          width: '100%',
-                          p: 1,
-                        }}
-                      >
-                        <Typography
-                          key={index}
-                          sx={{
-                            width: '100%',
-                            fontSize: '1.1rem',
-                            color: '#40403F',
-                            fontWeight: '600',
-                            textTransform: 'uppercase',
-                            wordBreak: 'break-all',
-                          }}
-                        >
-                          {task.description}
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </Box>
-                  <Box
+            {task?.every((item) => item.taskValue !== '1') ? (
+              <Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: '#c7c7c7',
+                  }}
+                >
+                  <AssignmentIcon
                     sx={{
-                      display: 'flex',
-                      width: '100%',
-                      justifyContent: 'flex-end',
+                      fontSize: '10rem',
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Typography
+                    variant="subtitle1"
+                    sx={{
+                      color: '#c7c7c7',
+                      textAlign: 'center',
+                      cursor: 'default',
                     }}
                   >
-                    <div>
-                      <Button
-                        color="success"
-                        onClick={() => handleTaksDone(task)}
+                    VOCÊ AINDA NÃO POSSUI NENHUMA TAREFA!
+                  </Typography>
+                </Box>
+              </Box>
+            ) : (
+              task
+                ?.filter((item) => item.taskValue === '1')
+                .map((task) => {
+                  return (
+                    <Box key={task.id.toString()}>
+                      <Box display="flex" key={task.id.toString()}>
+                        <Box
+                          sx={{
+                            display: 'flex',
+                            width: '100%',
+                            minHeight: '4rem',
+                            borderRadius: '10px',
+                            backgroundColor: '#cfcfcf',
+                            borderLeft: '10px solid #7B1EA2',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <Box
+                            display="flex"
+                            sx={{
+                              width: '100%',
+                              p: 1,
+                            }}
+                          >
+                            <Box>
+                              <Typography
+                                variant="h3"
+                                sx={{
+                                  width: '100%',
+                                  fontSize: '1.1rem',
+                                  color: '#40403F',
+                                  fontWeight: '600',
+                                  textTransform: 'uppercase',
+                                  wordBreak: 'break-all',
+                                }}
+                              >
+                                {task.description}
+                              </Typography>
+                            </Box>
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          width: '100%',
+                          justifyContent: 'flex-end',
+                        }}
                       >
-                        <CheckCircleIcon />
-                      </Button>
+                        <Box>
+                          <Button
+                            color="success"
+                            onClick={() => handleTaksDone(task)}
+                          >
+                            <CheckCircleIcon />
+                          </Button>
 
-                      <Button color="error">
-                        <DeleteForeverIcon />
-                      </Button>
-                    </div>
-                  </Box>
-                </>
-              )
-            })}
+                          <Button
+                            color="error"
+                            onClick={() => handleTaksCancel(task)}
+                          >
+                            <ClearIcon />
+                          </Button>
+                        </Box>
+                      </Box>
+                    </Box>
+                  )
+                })
+            )}
+
+            <Snackbar
+              open={open}
+              autoHideDuration={1500}
+              onClose={handleSnackClose}
+            >
+              <Alert
+                onClose={handleSnackClose}
+                severity="success"
+                sx={{ width: '100%' }}
+              >
+                Parabéns tarefa finalizada!
+              </Alert>
+            </Snackbar>
           </TabPanel>
-          <TabPanel value="2">Item Two</TabPanel>
-          <TabPanel value="3">Item Three</TabPanel>
+
+          {/* ///////// DONE */}
+
+          <TabPanel value="2">
+            {task?.every((item) => item.taskValue !== '2') ? (
+              <Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: '#c7c7c7',
+                  }}
+                >
+                  <GradingIcon
+                    sx={{
+                      fontSize: '10rem',
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Typography
+                    variant="subtitle1"
+                    sx={{
+                      color: '#c7c7c7',
+                      textAlign: 'center',
+                      cursor: 'default',
+                    }}
+                  >
+                    VOCÊ AINDA NÃO POSSUI NENHUMA TAREFA FINALIZADA!
+                  </Typography>
+                </Box>
+              </Box>
+            ) : (
+              task
+                ?.filter((item) => item.taskValue === '2')
+                .map((task) => {
+                  return (
+                    <Box key={task.id.toString()}>
+                      <Box display="flex">
+                        <Box
+                          sx={{
+                            display: 'flex',
+                            width: '100%',
+                            minHeight: '4rem',
+                            borderRadius: '10px',
+                            backgroundColor: '#cfcfcf',
+                            borderLeft: '10px solid #2F7D31',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <Box
+                            display="flex"
+                            sx={{
+                              width: '100%',
+                              p: 1,
+                            }}
+                          >
+                            <Typography
+                              variant="h3"
+                              sx={{
+                                width: '100%',
+                                fontSize: '1.1rem',
+                                color: '#40403F',
+                                fontWeight: '600',
+                                textTransform: 'uppercase',
+                                wordBreak: 'break-all',
+                              }}
+                            >
+                              {task.description}
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          width: '100%',
+                          justifyContent: 'flex-end',
+                        }}
+                      >
+                        <Box>
+                          <Button
+                            color="success"
+                            onClick={() => handleUndo(task)}
+                          >
+                            <UndoIcon />
+                          </Button>
+                        </Box>
+                      </Box>
+                    </Box>
+                  )
+                })
+            )}
+          </TabPanel>
+
+          {/* ///////// CANCEL */}
+
+          <TabPanel value="3">
+            {task?.every((item) => item.taskValue !== '2') ? (
+              <Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: '#c7c7c7',
+                  }}
+                >
+                  <DoDisturbAltIcon
+                    sx={{
+                      fontSize: '10rem',
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Typography
+                    variant="subtitle1"
+                    sx={{
+                      color: '#c7c7c7',
+                      textAlign: 'center',
+                      cursor: 'default',
+                    }}
+                  >
+                    VOCÊ AINDA NÃO POSSUI NENHUMA TAREFA CANCELADA!
+                  </Typography>
+                </Box>
+              </Box>
+            ) : (
+              task
+                ?.filter((item) => item.taskValue === '3')
+                .map((task) => {
+                  return (
+                    <Box key={task.id.toString()}>
+                      <Box display="flex" key={task.id.toString()}>
+                        <Box
+                          sx={{
+                            display: 'flex',
+                            width: '100%',
+                            minHeight: '4rem',
+                            borderRadius: '10px',
+                            backgroundColor: '#cfcfcf',
+                            borderLeft: '10px solid #D3302F',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <Box
+                            display="flex"
+                            sx={{
+                              width: '100%',
+                              p: 1,
+                            }}
+                          >
+                            <Typography
+                              variant="h3"
+                              sx={{
+                                width: '100%',
+                                fontSize: '1.1rem',
+                                color: '#40403F',
+                                fontWeight: '600',
+                                textTransform: 'uppercase',
+                                wordBreak: 'break-all',
+                              }}
+                            >
+                              {task.description}
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          width: '100%',
+                          justifyContent: 'flex-end',
+                        }}
+                      >
+                        <Box>
+                          <Button
+                            color="error"
+                            onClick={() => handleUndo(task)}
+                          >
+                            <UndoIcon />
+                          </Button>
+                        </Box>
+                      </Box>
+                    </Box>
+                  )
+                })
+            )}
+          </TabPanel>
         </TabContext>
       </Container>
     </Box>
   )
 }
+
+export default dynamic(() => Promise.resolve(Home), { ssr: false })
